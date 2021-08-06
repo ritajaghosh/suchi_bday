6b231ca3cfd8a5db7271b8104cb8b2d3d052de5d
sukantahui <huisukanta77@gmail.com>
2021-08-06 10:21:05 +0530

    little edit

--- src/app/pages/home/why-us/why-us.component.ts
+++ src/app/pages/home/why-us/why-us.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./why-us.component.scss']
 })
 export class WhyUsComponent implements OnInit {
-
+  avatar = {ritja: 'https://live.staticflickr.com/65535/50588567573_8d9e8f1502_o.jpg'};
   constructor() { }
 
   ngOnInit(): void {
