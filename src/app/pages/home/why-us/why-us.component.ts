022cb654bbdf5dcbdde499a0eb1b5497ab7b95fc
Ritaja Ghosh <ritajaghosh@gmail.com>
2021-08-08 20:38:11 +0530

    edit project

--- src/app/pages/home/why-us/why-us.component.ts
+++ src/app/pages/home/why-us/why-us.component.ts
@@ -11,7 +11,13 @@ export class WhyUsComponent implements OnInit {
     ritja: 'https://live.staticflickr.com/65535/50588567573_8d9e8f1502_o.jpg',
     mou: 'https://live.staticflickr.com/65535/50589328831_be485ed9ce.jpg',
     piyali: 'https://live.staticflickr.com/65535/51363382192_426b3a6d95_t.jpg',
-    sukanta: 'https://live.staticflickr.com/65535/51363382192_426b3a6d95_t.jpg'
+    sukanta: 'https://live.staticflickr.com/65535/51365683039_52303039b3_t.jpg',
+    anirban: 'https://live.staticflickr.com/65535/51363382242_b95b668f27_t.jpg',
+    rumila:   'https://live.staticflickr.com/65535/51364148907_0ed25ecd0c_t.jpg',
+    salini:   'https://live.staticflickr.com/65535/51363435432_b7d34d1ac2_t.jpg',
+    didi:      'https://live.staticflickr.com/65535/51365153500_73c1f70b0f_t.jpg',
+    ahana :  'https://live.staticflickr.com/65535/51364877759_2f96748dc9_t.jpg',
+
   };
   videoURL = 'https://www.youtube.com/embed/xBXI-VKmXvQ';
   private safeURL: SafeResourceUrl;
